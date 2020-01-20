fd5fe8df9a166cce6352418026361ec5c20cbe8f
Jean C. Solís <jcsolisu2000@gmail.com>
2020-01-19 19:16:24 -0600

    Build with Webpack

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 Remove UTM
 ======
 - Chrome Extension to remove UTM parameters from URL.
+
+## Build
+
+npx webpack --config webpack.config.js
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const ZipPlugin = require('zip-webpack-plugin');
+
+const rootPath = path.resolve(__dirname);
+
+module.exports = {
+    mode: 'none', // "production" | "development" | "none"
+    entry: './utm.js',
+    output: {
+        path: path.join(rootPath, 'dist')
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new CopyPlugin(
+            [{ from: path.join(rootPath, '*.png'), to: path.join(rootPath, 'dist') },
+            { from: 'manifest.json', to: path.join(__dirname, 'dist') },
+            { from: path.join(rootPath, '_locales'), to: path.join(rootPath, 'dist/_locales') }]
+        ),
+        new ZipPlugin({
+            path: path.join(rootPath, 'dist'),
+            filename: 'remove-utm.zip'
+        })
+    ]
+};
