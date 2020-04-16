99c274d7b4b68b8d7e15197eec161fb84e8f7013
Jean C. Solís <jcsolisu2000@gmail.com>
2020-04-15 21:12:02 -0600

    Add cid_source, cid_medium, cid, cid_content

--- utm.js
+++ utm.js
@@ -1,4 +1,5 @@
-const params = ['fbclid', 'utm_campaign', 'utm_content', 'utm_medium', 'utm_source', 'utm_term', 'sc_cid', 'WT.mc_id', 'hss_channel'];
+const params = ['fbclid', 'utm_campaign', 'utm_content', 'utm_medium', 'utm_source', 'utm_term', 'sc_cid', 'WT.mc_id', 'hss_channel',
+    'cid_source', 'cid_medium', 'cid', 'cid_content'];
 
 const url = new URL(location.href);
 params.forEach((param) => {
